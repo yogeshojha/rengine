c17712f744e3e9268ed23c833b0fd355979acc39
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-05-05 23:19:31 +0530

    postgresql configured

--- reNgine/settings.py
+++ reNgine/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = 'reNgine.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
