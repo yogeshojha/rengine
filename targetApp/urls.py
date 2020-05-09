b9fff8baf3fb43ffe2edcba737576fff4edf5878
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-05-09 09:59:42 +0530

    migrated from add_target_db to normal form

--- targetApp/urls.py
+++ targetApp/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('', views.index, name='targetIndex'),
     path('add/', views.add_target_form, name='add_target_form'),
     path('list/', views.list_target, name='list_target'),
-    path('add_target_db/', views.add_target_db, name='add_target_db'),
 ]
--- targetApp/views.py
+++ targetApp/views.py
@@ -9,19 +9,15 @@ def index(request):
     return render(request, 'target/index.html')
 
 def add_target_form(request):
-    context = {"add_target_li": "active", "target_data_active": "true"}
-    return render(request, 'target/add.html', context)
-
-def list_target(request):
-    context = {"list_target_li": "active", "target_data_active": "true"}
-    return render(request, 'target/list.html', context)
-
-def add_target_db(request):
     if request.method == "POST":
         domain = Domain()
         domain.domain_name = request.POST.get('domainName')
         domain.domain_description = request.POST.get('domainDescription')
         domain.save_domain()
         messages.add_message(request, messages.INFO, 'Target domain ' + domain.domain_name + ' added successfully')
-    # exception handling
-    return render(request, 'target/add.html')
+    context = {"add_target_li": "active", "target_data_active": "true"}
+    return render(request, 'target/add.html', context)
+
+def list_target(request):
+    context = {"list_target_li": "active", "target_data_active": "true"}
+    return render(request, 'target/list.html', context)
