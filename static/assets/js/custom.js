367b286ad55dbc3e2cf6bc12b0614ae6eea63984
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-05-10 15:53:30 +0530

    custom js files recreate

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -1,10 +1,4 @@
-/*
-=========================================
-|                                       |
-|           Scroll To Top               |
-|                                       |
-=========================================
-*/
+
 $('.scrollTop').click(function() {
     $("html, body").animate({scrollTop: 0});
 });
@@ -13,14 +7,6 @@ $('.scrollTop').click(function() {
 $('.navbar .dropdown.notification-dropdown > .dropdown-menu, .navbar .dropdown.message-dropdown > .dropdown-menu ').click(function(e) {
     e.stopPropagation();
 });
-
-/*
-=========================================
-|                                       |
-|       Multi-Check checkbox            |
-|                                       |
-=========================================
-*/
 
 function checkall(clickchk, relChkbox) {
 
@@ -33,18 +19,6 @@ function checkall(clickchk, relChkbox) {
     });
 }
 
-
-/*
-=========================================
-|                                       |
-|           MultiCheck                  |
-|                                       |
-=========================================
-*/
-
-/*
-    This MultiCheck Function is recommanded for datatable
-*/
 
 function multiCheck(tb_var) {
     tb_var.on("change", ".chk-parent", function() {
@@ -57,15 +31,6 @@ function multiCheck(tb_var) {
         $(this).parents("tr").toggleClass("active")
     })
 }
-
-/*
-=========================================
-|                                       |
-|           MultiCheck                  |
-|                                       |
-=========================================
-*/
-
 function checkall(clickchk, relChkbox) {
 
     var checker = $('#' + clickchk);
@@ -77,45 +42,13 @@ function checkall(clickchk, relChkbox) {
     });
 }
 
-/*
-=========================================
-|                                       |
-|               Tooltips                |
-|                                       |
-=========================================
-*/
-
 $('.bs-tooltip').tooltip();
 
-/*
-=========================================
-|                                       |
-|               Popovers                |
-|                                       |
-=========================================
-*/
-
 $('.bs-popover').popover();
-
-
-/*
-================================================
-|                                              |
-|               Rounded Tooltip                |
-|                                              |
-================================================
-*/
 
 $('.t-dot').tooltip({
     template: '<div class="tooltip status rounded-tooltip" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
 })
-
-
-/*
-================================================
-|            IE VERSION Dector                 |
-================================================
-*/
 
 function GetIEVersion() {
   var sAgent = window.navigator.userAgent;
@@ -131,4 +64,55 @@ function GetIEVersion() {
 
   else
     return 0; //It is not IE
+}
+
+function delete_domain(id, domain_name)
+{
+    const delAPI = "../delete/"+id;
+    swal.queue([{
+        title: 'Are you sure you want to delete '+ domain_name +'?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Delete',
+        padding: '2em',
+        showLoaderOnConfirm: true,
+        preConfirm: function() {
+          return fetch(delAPI, {
+	            method: 'POST',
+                credentials: "same-origin",
+                headers: {
+                    "X-CSRFToken": getCookie("csrftoken")
+                }
+            })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function(data) {
+                // TODO Look for better way
+               return location.reload();
+            })
+            .catch(function() {
+              swal.insertQueueStep({
+                type: 'error',
+                title: 'Oops! Unable to delete the domain!'
+              })
+            })
+        }
+    }])
+}
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
