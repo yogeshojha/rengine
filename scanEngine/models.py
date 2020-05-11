65c1c64971f692ed0058866e89e0232ac472db93
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-05-11 19:25:56 +0530

    model for multiple checkbox created, docker pg db exposed

--- scanEngine/models.py
+++ scanEngine/models.py
@@ -1,2 +1,13 @@
 from django.db import models
-# Create your models here.
+from multiselectfield import MultiSelectField
+
+SCAN_TYPES = (('subdomain_discovery', 'Subdomain Discovery'),
+              ('visual_identification', 'Visual Idenfitication'),
+              ('dir_file_search', 'Directory & Files Discovery'),
+              ('subdomain_takeover', 'Subdomain Takeover'),
+              ('param_discovery', 'Parameter Discovery'),
+              ('port_scan', "Port Scan"))
+
+class EngineType(models.Model):
+    scan_type_name = models.CharField(max_length=200)
+    my_field = MultiSelectField(choices=SCAN_TYPES)
