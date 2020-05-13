6885a0967e20effd53608206276a49a348c5fa44
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-05-13 20:01:24 +0530

    models migrated

--- startScan/admin.py
+++ startScan/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from startScan.models import SubdomainModel
 
-# Register your models here.
+admin.site.register(SubdomainModel)
--- startScan/models.py
+++ startScan/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class SubdomainModel(models.Model):
+    hello = models.CharField(max_length=500)
