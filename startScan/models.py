b74a1f772575f8adc879fad0f5b9a35424f35370
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-05-13 22:38:10 +0530

    one to many relationship established

--- startScan/models.py
+++ startScan/models.py
@@ -3,6 +3,9 @@ from targetApp.models import Domain
 from scanEngine.models import EngineType
 
 class SubdomainModel(models.Model):
-    
+    last_scan_date = models.DateTimeField()
+    domain_name = models.ForeignKey(Domain, on_delete=models.CASCADE)
+    scan_type = models.ForeignKey(EngineType, on_delete=models.CASCADE)
+
     def __str__(self):
-        return self.domain_name
+        return self.domain_name.domain_name
