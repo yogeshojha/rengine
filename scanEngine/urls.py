14761f8004ac3e8f0df0c7ef5990adbd188905ce
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-05-13 19:01:01 +0530

    delete engine working

--- scanEngine/urls.py
+++ scanEngine/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='scan_engine_index'),
     path('add', views.add_engine, name='add_engine'),
+    path('delete/<int:id>', views.delete_engine, name='delete_engine_url'),
 ]
