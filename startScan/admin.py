1ecc4a18a86ad68392fe4da0997a56de253845cb
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-05-14 00:11:01 +0530

    scanned subdomains model

--- startScan/admin.py
+++ startScan/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from startScan.models import ScanHistoryModel
+from startScan.models import ScanHistoryModel, ScannedSubdomains
 
 admin.site.register(ScanHistoryModel)
+admin.site.register(ScannedSubdomains)
--- startScan/models.py
+++ startScan/models.py
@@ -9,4 +9,14 @@ class ScanHistoryModel(models.Model):
     scan_type = models.ForeignKey(EngineType, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.domain_name.domain_name
+        return self.domain_name.domain_name + str(self.id)
+
+class ScannedSubdomains(models.Model):
+    subdomain = models.CharField(max_length=100)
+    domain_name = models.ForeignKey(ScanHistoryModel, on_delete=models.CASCADE)
+    open_ports = models.CharField(max_length=1000)
+    takeover_possible = models.BooleanField()
+    technology_stack = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.subdomain
