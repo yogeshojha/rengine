d66871de4240344223d2ebbf8b047a700d06dac9
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-05-26 18:12:57 +0530

    better way to look for subdomains inside saving aquatone results

--- startScan/views.py
+++ startScan/views.py
@@ -55,54 +55,56 @@ def start_scan_ui(request, id):
     return render(request, 'startScan/start_scan_ui.html', context)
 
 def doScan(id, domain):
-    task = ScanHistory.objects.get(pk=id)
-    notif_hook = NotificationHooks.objects.filter(send_notif=True)
-    results_dir = '/app/tools/scan_results/'
-    os.chdir(results_dir)
     try:
-        current_scan_dir = domain.domain_name+'__'+str(datetime.strftime(timezone.now(), '%Y_%m_%d_%H_%M_%S'))
-        os.mkdir(current_scan_dir)
-    except:
-        # do something here
-        print("Oops!")
-    # all scan happens here
-    os.system('/app/tools/get_subdomain.sh %s %s' %(domain.domain_name, current_scan_dir))
+        task = ScanHistory.objects.get(pk=id)
+        notif_hook = NotificationHooks.objects.filter(send_notif=True)
+        results_dir = '/app/tools/scan_results/'
+        os.chdir(results_dir)
+        try:
+            current_scan_dir = domain.domain_name+'__'+str(datetime.strftime(timezone.now(), '%Y_%m_%d_%H_%M_%S'))
+            os.mkdir(current_scan_dir)
+        except:
+            # do something here
+            print("Oops!")
+        # all scan happens here
+        os.system('/app/tools/get_subdomain.sh %s %s' %(domain.domain_name, current_scan_dir))
 
-    scan_results_file = results_dir+current_scan_dir+'/sorted_subdomain_collection.txt'
-    with open(scan_results_file) as subdomain_list:
-        for subdomain in subdomain_list:
-            scanned = ScannedHost()
-            scanned.subdomain = subdomain.rstrip('\n')
-            scanned.scan_history = task
-            scanned.takeover_possible = False
-            scanned.save()
-    # after subdomain discovery run aquatone for visual identification
-    with_protocol_path = results_dir + current_scan_dir + '/with_protocol_domains.txt'
-    output_aquatone_path = results_dir + current_scan_dir + '/aquascreenshots/'
-    aquatone_command = 'cat {} | /app/tools/aquatone -ports xlarge -out {}'.format(with_protocol_path, output_aquatone_path)
-    os.system(aquatone_command)
+        scan_results_file = results_dir+current_scan_dir+'/sorted_subdomain_collection.txt'
+        with open(scan_results_file) as subdomain_list:
+            for subdomain in subdomain_list:
+                scanned = ScannedHost()
+                scanned.subdomain = subdomain.rstrip('\n')
+                scanned.scan_history = task
+                scanned.takeover_possible = False
+                scanned.save()
+        # after subdomain discovery run aquatone for visual identification
+        with_protocol_path = results_dir + current_scan_dir + '/with_protocol_domains.txt'
+        output_aquatone_path = results_dir + current_scan_dir + '/aquascreenshots/'
+        aquatone_command = 'cat {} | /app/tools/aquatone -ports xlarge -out {}'.format(with_protocol_path, output_aquatone_path)
+        os.system(aquatone_command)
 
-    aqua_json_path = output_aquatone_path + '/aquatone_session.json'
-    with open(aqua_json_path, 'r') as json_file:
-        data = json.load(json_file)
-    subdomain_details = ScannedHost.objects.filter(scan_history__id=id)
+        aqua_json_path = output_aquatone_path + '/aquatone_session.json'
+        with open(aqua_json_path, 'r') as json_file:
+            data = json.load(json_file)
 
-    # should be a better way for this
-    for sub in subdomain_details:
         for host in data['pages']:
-            if sub.subdomain == data['pages'][host]['hostname']:
-                subdomain_proto = ScannedSubdomainWithProtocols()
-                subdomain_proto.host = sub
-                subdomain_proto.url = data['pages'][host]['url']
-                list_ip = data['pages'][host]['addrs']
-                ip_string = ','.join(list_ip)
-                subdomain_proto.ip_address = ip_string
-                subdomain_proto.page_title = data['pages'][host]['pageTitle']
-                subdomain_proto.http_status = data['pages'][host]['status'][0:3]
-                subdomain_proto.screenshot_path = current_scan_dir + '/aquascreenshots/' + data['pages'][host]['screenshotPath']
-                subdomain_proto.technology_stack = 'fake'
-                subdomain_proto.save()
+            subdomain_details = ScannedHost.objects.get(scan_history__id=id, subdomain=data['pages'][host]['hostname'])
+            subdomain_proto = ScannedSubdomainWithProtocols()
+            subdomain_proto.host = subdomain_details
+            subdomain_proto.url = data['pages'][host]['url']
+            list_ip = data['pages'][host]['addrs']
+            ip_string = ','.join(list_ip)
+            subdomain_proto.ip_address = ip_string
+            subdomain_proto.page_title = data['pages'][host]['pageTitle']
+            subdomain_proto.http_status = data['pages'][host]['status'][0:3]
+            subdomain_proto.screenshot_path = current_scan_dir + '/aquascreenshots/' + data['pages'][host]['screenshotPath']
+            subdomain_proto.technology_stack = data['pages'][host]['tags']
+            subdomain_proto.save()
 
+
+        task.scan_status = 2
+    except:
+        task.scan_status = 0
     task.scan_status = 2
     task.save()
     # notify on slack
