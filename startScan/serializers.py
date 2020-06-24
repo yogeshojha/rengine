998eb1c79d144f035a249f95b6ee5daec66e220f
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-06-24 22:02:54 +0530

    drf filter

--- startScan/serializers.py
+++ startScan/serializers.py
@@ -4,13 +4,8 @@ from .models import ScannedHost, ScanHistory
 
 class ScannedHostSerializer(serializers.ModelSerializer):
 
-    # scan_history = serializers.SerializerMethodField()
-    #
-    # def get_scan_history(self, scanned_host):
-    #     return scanned_host.scan_history.domain_name.domain_name
-
     id = serializers.IntegerField(read_only=True)
-
+    
     class Meta:
         model = ScannedHost
         fields = '__all__'
