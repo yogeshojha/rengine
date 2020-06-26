a8d8150095fbc6b31db84a530f62b4d8b854ed00
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-06-26 10:24:47 +0530

    django rest framework api pk pass from get parameter

--- startScan/api/urls.py
+++ startScan/api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.urls import path
 from rest_framework import routers
-from .views import ScanHistoryViewSet, api_scan_host_detailed_view
+from .views import ScanHistoryViewSet
 
 app_name = 'startScan'
 router = routers.DefaultRouter()
@@ -10,7 +10,6 @@ router.register(r'scanHistory', ScanHistoryViewSet)
 
 urlpatterns = [
     url('^', include(router.urls)),
-    path('detail/<id>/', api_scan_host_detailed_view)
 
 ]
 
--- startScan/api/views.py
+++ startScan/api/views.py
@@ -5,24 +5,15 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, action
 
-@api_view(['GET', ])
-def api_scan_host_detailed_view(request, id):
-    try:
-        scanned_host = ScannedHost.objects.filter(scan_history__id=id)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    serializer = ScanHistorySerializer(scanned_host, many=True)
-    return Response(serializer.data)
-
-
 class ScanHistoryViewSet(viewsets.ModelViewSet):
     queryset = ScannedHost.objects.all()
     serializer_class = ScanHistorySerializer
 
-    @action(detail=True)
-    def scan_list(self, request, pk=None):
-        scan_history = ScannedHost.objects.filter(scan_history__id=pk)
-        scan_json = ScanHistorySerializer(scan_history, many=True)
-        return Response(scan_json.data)
-    # lookup_field = 'scan_history'
+    def get_queryset(self):
+        req = self.request
+        scan_id = req.query_params.get('scan_id')
+        if scan_id:
+            self.queryset = ScannedHost.objects.filter(scan_history__id=scan_id)
+            return self.queryset
+        else:
+            return self.queryset
