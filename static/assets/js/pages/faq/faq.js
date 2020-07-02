f993955c327cbd9708e18a4e58420690402e09f1
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-07-02 11:13:46 +0530

    removal of pluging and css

--- static/assets/js/pages/faq/faq.js
+++
@@ -1,7 +0,0 @@
-// Scroll To Top
-
-$(document).on('click', '.arrow', function(event) {
-  event.preventDefault();
-  var body = $("html, body");
-  body.stop().animate({scrollTop:0}, 500, 'swing');
-});
