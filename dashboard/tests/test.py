1aa5b8c0a35df5ff887de0dbf4a795b0af2e74d2
Aju100 <ajutamang10@outlook.com>
2020-07-06 16:48:37 +0545

    Updated test

--- dashboard/tests/test.py
+++ dashboard/tests/test.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
 from dashboard.views import index
-from django.http import HttpRequest
 
 class DashboardPageTest(TestCase):
 
@@ -11,14 +10,3 @@ class DashboardPageTest(TestCase):
     def test_dashboard_url_resolves_to_dashboard_view(self):
         found = resolve('/')
         self.assertEqual(found.func,index)
-
-'''
-
-    def test_dashboard_url_returns_correct_template(self):
-        request = HttpRequest()  
-        response = index(request)  
-        html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))  
-        self.assertIn('<title></title>', html)  
-        self.assertTrue(html.endswith('</html>'))  
-'''
