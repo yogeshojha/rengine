8a5dfd56373f9a7e4bf198bb69041501ec52f2ec
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-07-06 22:11:19 +0530

    scan based on selection

--- startScan/urls.py
+++ startScan/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name="start_scan"),
     path('history/', views.scan_history, name="scan_history"),
     path('detail/<int:id>', views.detail_scan, name='detail_scan'),
-    path('start/<int:id>', views.start_scan_ui, name='start_scan'),
+    path('start/<int:host_id>', views.start_scan_ui, name='start_scan'),
     path('api/', include('startScan.api.urls', 'scan_host_api')),
 ]
