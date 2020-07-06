383509c3be1d2fad7f49d9f05835fce4d0e421ce
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-07-07 00:12:38 +0530

    removed go binaries

--- tools/get_subdomain.sh
+++ tools/get_subdomain.sh
@@ -1,7 +1,7 @@
 # python3 /app/tools/Sublist3r/sublist3r.py -d $1 -t 10 -o $2/from_sublister.txt
 # /app/tools/amass enum --passive -d $1 -o $2/fromamass.txt
-# /app/tools/assetfinder --subs-only $1 > $2/fromassetfinder.txt
-/app/tools/subfinder -d $1 > $2/fromsubfinder.txt
+# assetfinder --subs-only $1 > $2/fromassetfinder.txt
+subfinder -d $1 > $2/fromsubfinder.txt
 cat $2/*.txt > $2/subdomain_collection.txt
 rm -rf $2/from*
 sort -u $2/subdomain_collection.txt -o $2/sorted_subdomain_collection.txt
