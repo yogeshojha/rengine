e99e91ed20c2b4534c5907dc25b85706802a928e
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-07-07 11:56:23 +0530

    fixtures loaded by default

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 
 python manage.py migrate
+
+# Load default engine types
+python manage.py loaddata fixtures/default_scan_engines.json --app scanEngine.EngineType
+
 # python manage.py collectstatic --no-input --clear
 
 exec "$@"
