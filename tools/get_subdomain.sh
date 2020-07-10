38e980ec7c193d1a27c12294514e9b473d82e603
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-07-10 20:40:17 +0530

    delete scan results

--- tools/get_subdomain.sh
+++ tools/get_subdomain.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# python3 /app/tools/Sublist3r/sublist3r.py -d $1 -t 10 -o $2/from_sublister.txt
-# /app/tools/amass enum --passive -d $1 -o $2/fromamass.txt
-# assetfinder --subs-only $1 > $2/fromassetfinder.txt
+python3 /app/tools/Sublist3r/sublist3r.py -d $1 -t 10 -o $2/from_sublister.txt
+/app/tools/amass enum --passive -d $1 -o $2/fromamass.txt
+assetfinder --subs-only $1 > $2/fromassetfinder.txt
 subfinder -d $1 > $2/fromsubfinder.txt
 cat $2/*.txt > $2/subdomain_collection.txt
 rm -rf $2/from*
