29c904e6abd7db1806b6e296a2a0fc4c1b4b4c72
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-07-12 19:02:03 +0530

    migrated to lightweight alpine

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z db 5432; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
 
 python manage.py migrate
 
