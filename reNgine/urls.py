794f9fe1656149b3c98d69c349fdf54ed18a7013
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-07-20 00:30:43 +0530

    login imported

--- reNgine/urls.py
+++ reNgine/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path(
@@ -22,6 +23,14 @@ urlpatterns = [
     path(
         'notification/',
         include('notification.urls')),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='base/login.html'),
+        name='login'),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(template_name='base/login.html'),
+        name='logout'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
     static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
