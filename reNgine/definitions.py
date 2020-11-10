54354c05410ac4479e5c720b52a5d647dbd5b461
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-11-10 18:39:43 +0530

    Definitions Added

---
+++ reNgine/definitions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+###############################################################################
+# TOOLS DEFINITIONS
+###############################################################################
+
+AMASS_COMMAND = '/app/tools/amass enum'
+###############################################################################
+# YAML CONFIG DEFINITIONS
+###############################################################################
+
+AMASS_CONFIG = 'amass_config'
+AMASS_WORDLIST = 'amass_wordlist'
+
+SUBDOMAIN_DISCOVERY = 'subdomain_discovery'
+THREAD = 'thread'
+USES_TOOLS = 'uses_tool'
+
+###############################################################################
+# Wordlist DEFINITIONS
+###############################################################################
+AMASS_DEFAULT_WORDLIST_PATH = 'wordlist/default_wordlist/deepmagic.com-prefixes-top50000.txt'
