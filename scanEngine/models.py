f7759bf6564128f6ae855d2cd25d1c2424cdbf0e
Yogesh Ojha <yogesh.ojha11@gmail.com>
2020-11-28 00:57:06 +0530

    Progressbar added

--- scanEngine/models.py
+++ scanEngine/models.py
@@ -7,12 +7,21 @@ class EngineType(models.Model):
     dir_file_search = models.BooleanField()
     port_scan = models.BooleanField()
     fetch_url = models.BooleanField()
-    yaml_configuration = models.TextField()
     vulnerability_scan = models.BooleanField(null=True, default=False)
+    yaml_configuration = models.TextField()
     default_engine = models.BooleanField(null=True, default=False)
 
     def __str__(self):
         return self.engine_name
+
+    def get_number_of_steps(self):
+        engine_list = [
+            self.subdomain_discovery,
+            self.dir_file_search,
+            self.port_scan,
+            self.fetch_url,
+            self.vulnerability_scan]
+        return sum(bool(item) for item in engine_list)
 
 
 class Wordlist(models.Model):
