f53fece553756e66a91d812f3e604962fd951a2f
Yogesh Ojha <yogesh.ojha11@gmail.com>
2021-03-03 10:46:23 +0530

    Added vulnerability and endpoint count and difference

--- reNgine/tasks.py
+++ reNgine/tasks.py
@@ -590,6 +590,7 @@ def doScan(domain_id, scan_history_id, scan_type, engine_type):
                                 endpoint.http_status = json_st['status-code']
                                 endpoint.page_title = json_st['title']
                                 endpoint.discovered_date = timezone.now()
+                                endpoint.target_domain = domain
                                 if 'content-type' in json_st:
                                     endpoint.content_type = json_st['content-type']
                                 endpoint.save()
@@ -729,6 +730,7 @@ def doScan(domain_id, scan_history_id, scan_type, engine_type):
                             vulnerability.matcher_name = json_st['matcher_name']
                         vulnerability.discovered_date = timezone.now()
                         vulnerability.open_status = True
+                        vulnerability.target_domain = domain
                         vulnerability.save()
                         send_notification(
                             "ALERT! {} vulnerability with {} severity identified in {} \n Vulnerable URL: {}".format(
