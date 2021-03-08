070cea4274a2df40d6692df506b813e3a3d43cdc
Yogesh Ojha <yogesh.ojha11@gmail.com>
2021-03-08 23:00:56 +0530

    Interesting Endpoint added

--- reNgine/common_func.py
+++ reNgine/common_func.py
@@ -2,30 +2,71 @@
 from django.db.models import Q
 from functools import reduce
 from scanEngine.models import InterestingLookupModel
-from startScan.models import ScannedHost
+from startScan.models import ScannedHost, WayBackEndPoint
 
 
-def get_interesting_subdomains(scan_history=None, target=None):
-    default_lookup_keywords = [key.strip() for key in InterestingLookupModel.objects.get(id=1).keywords.split(',')]
+def get_lookup_keywords():
+    default_lookup_keywords = [
+        key.strip() for key in InterestingLookupModel.objects.get(
+            id=1).keywords.split(',')]
     custom_lookup_keywords = []
     if InterestingLookupModel.objects.filter(custom_type=True):
-        custom_lookup_keywords = [key.strip() for key in InterestingLookupModel.objects.filter(custom_type=True).order_by('-id')[0].keywords.split(',')]
+        custom_lookup_keywords = [
+            key.strip() for key in InterestingLookupModel.objects.filter(
+                custom_type=True).order_by('-id')[0].keywords.split(',')]
     lookup_keywords = default_lookup_keywords + custom_lookup_keywords
     # remove empty strings from list, if any
     lookup_keywords = list(filter(None, lookup_keywords))
-    
+
+    return lookup_keywords
+
+
+def get_interesting_subdomains(scan_history=None, target=None):
+    lookup_keywords = get_lookup_keywords()
+
     subdomain_lookup_query = Q()
     page_title_lookup_query = Q()
 
     for key in lookup_keywords:
         subdomain_lookup_query |= Q(subdomain__icontains=key)
-        page_title_lookup_query |= Q(page_title__iregex="\y{}\y".format(key))
+        page_title_lookup_query |= Q(page_title__iregex="\\y{}\\y".format(key))
 
     if target:
-        subdomain_lookup = ScannedHost.objects.filter(target_domain__id=target).filter(subdomain_lookup_query)
-        title_lookup = ScannedHost.objects.filter(page_title_lookup_query).filter(target_domain__id=target)
+        subdomain_lookup = ScannedHost.objects.filter(
+            target_domain__id=target).filter(subdomain_lookup_query)
+        title_lookup = ScannedHost.objects.filter(
+            page_title_lookup_query).filter(target_domain__id=target)
     elif scan_history:
-        subdomain_lookup = ScannedHost.objects.filter(scan_history__id=scan_history).filter(subdomain_lookup_query)
-        title_lookup = ScannedHost.objects.filter(scan_history__id=scan_history).filter(page_title_lookup_query)
+        subdomain_lookup = ScannedHost.objects.filter(
+            scan_history__id=scan_history).filter(subdomain_lookup_query)
+        title_lookup = ScannedHost.objects.filter(
+            scan_history__id=scan_history).filter(page_title_lookup_query)
 
     return subdomain_lookup | title_lookup
+
+
+def get_interesting_endpoint(scan_history=None, target=None):
+    lookup_keywords = get_lookup_keywords()
+
+    url_lookup_query = Q()
+    page_title_lookup_query = Q()
+
+    for key in lookup_keywords:
+        url_lookup_query |= Q(http_url__icontains=key)
+        page_title_lookup_query |= Q(page_title__iregex="\\y{}\\y".format(key))
+
+    print(url_lookup_query)
+    print(page_title_lookup_query)
+
+    if target:
+        url_lookup = WayBackEndPoint.objects.filter(
+            target_domain__id=target).filter(url_lookup_query)
+        title_lookup = WayBackEndPoint.objects.filter(
+            target_domain__id=target).filter(page_title_lookup_query)
+    elif scan_history:
+        url_lookup = WayBackEndPoint.objects.filter(
+            url_of__id=scan_history).filter(url_lookup_query)
+        title_lookup = WayBackEndPoint.objects.filter(
+            url_of__id=scan_history).filter(page_title_lookup_query)
+
+    return url_lookup | title_lookup
---
+++ static/assets/js/ui-accordions.js
@@ -0,0 +1,17 @@
+$('#toggleAccordion .collapse').on('shown.bs.collapse', function(){
+    $(this).parent().find(".icons").html('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>');
+}).on('hidden.bs.collapse', function(){
+    $(this).parent().find(".icons").html('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>');
+});
+
+$('#withoutSpacing .collapse').on('shown.bs.collapse', function(){
+    $(this).parent().find(".icons").html('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>');
+}).on('hidden.bs.collapse', function(){
+    $(this).parent().find(".icons").html('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>');
+});
+
+$('#iconsAccordion .collapse').on('shown.bs.collapse', function(){
+    $(this).parent().find(".icons").html('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>');
+}).on('hidden.bs.collapse', function(){
+    $(this).parent().find(".icons").html('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>');
+});
