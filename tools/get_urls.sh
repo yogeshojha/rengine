21f1780e3af122689f49d69ad2db37c1fc8d7489
Yogesh Ojha <yogesh.ojha11@gmail.com>
2021-03-13 18:20:25 +0530

    Endpoints to vulnerability scan

--- tools/get_urls.sh
+++ tools/get_urls.sh
@@ -16,7 +16,12 @@ cat $2/httpx* > $2/final_httpx_urls.json
 
 cat $2/url* >> $2/all_urls.txt
 
+# Sort and unique the endpoints
 sort -u $2/all_urls.txt -o $2/all_urls.txt
+
+# unfurl the urls to keep only domain and path
+cat $2/all_urls.txt | unfurl -u format %s://%d%p >> $2/unfurl_urls.txt
+sort -u $2/unfurl_urls.txt -o $2/unfurl_urls.txt
 
 rm -rf $2/url*
 rm -rf $2/httpx*
