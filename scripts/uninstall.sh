a38a75ae8a1ea4d4085f85933bd4919973f4be1d
Yogesh Ojha <yogesh.ojha11@gmail.com>
2021-06-26 11:55:03 +0530

    Added option to uninstall reNgine

---
+++ scripts/uninstall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+if [ "$EUID" -ne 0 ]
+  then
+  echo "Error uninstalling reNgine, Please run this script as root!"
+  echo "Example: sudo ./uninstall.sh"
+  exit
+fi
+
+cat text_art.txt
+echo "Uninstalling reNgine"
+
+echo "Stopping reNgine"
+docker stop rengine_web_1 rengine_db_1 rengine_celery_1 rengine_celery-beat_1 rengine_redis_1
+
+echo "Removing all Containers related to reNgine"
+docker rm rengine_web_1 rengine_db_1 rengine_celery_1 rengine_celery-beat_1 rengine_redis_1
+echo "Removed all Containers"
+
+echo "Removing All volumes related to reNgine"
+docker volume rm rengine_gf_patterns rengine_github_repos rengine_nuclei_templates rengine_postgres_data rengine_scan_results
+echo "Removed all Volumes"
+
+echo "Removing all networks related to reNgine"
+docker network rm rengine_rengine_network
+
+echo "Finished Uninstalling."
