7cfb707830c884f12d951d84181b1344c84ea0c6
Yogesh Ojha <yogesh.ojha11@gmail.com>
2021-07-04 01:25:42 +0530

    Added Expand nodes filter

--- web/static/custom/mitch.js
+++ web/static/custom/mitch.js
@@ -54,43 +54,61 @@ function visualise_scan_results(scan_id)
     }
 
     updateTreeClasses(treePlugin);
-    
+
+    var checkbox = document.querySelector("input[name=expand-nodes-checkbox]");
+
+    checkbox.addEventListener('change', function() {
+      if (this.checked) {
+        var nodes = treePlugin.getNodes();
+        nodes.forEach(function(node, index, arr) {
+          treePlugin.expand(node);
+        });
+        treePlugin.update(treePlugin.getRoot());
+      } else {
+        var nodes = treePlugin.getNodes();
+        nodes.forEach(function(node, index, arr) {
+          treePlugin.collapse(node);
+        });
+        treePlugin.update(treePlugin.getRoot());
+      }
+    });
+
   }).fail(function(){
     $('#visualisation-loader').empty();
     $("#visualisation-loader").append(`<h5 class="text-danger">Sorry, could not visualize.</h5>`);
   });
+}
 
-  function updateTreeClasses(treePlugin)
-  {
-    treePlugin.getPanningContainer().selectAll("g.node")
-    .attr("class", function(data, index, arr) {
-      bg_class = '';
-      var depthClass = "depth-" + data.depth;
-      var existingClasses = this.getAttribute('class');
-      if (data.data.http_status >= 400 || data.data.is_uncommon || data.data.description == 'High') {
-        bg_class = " danger-box "
-      }
-      else if (data.data.http_status > 200 && data.data.http_status < 400 || data.data.description == 'Medium') {
-        bg_class = " warning-box "
-      }
-      else if (data.data.http_status == 200) {
-        bg_class = " success-box "
-      }
-      else if (data.data.description == 'Critical'){
-        bg_class = " critical-box "
-      }
-      else if (data.data.description == 'Low'){
-        bg_class = " low-box "
-      }
-      else if (data.data.description == 'Informational'){
-        bg_class = " info-box "
-      }
-      if (!existingClasses)
-      return depthClass;
-      var hasDepthClassAlready = (' ' + existingClasses + ' ').indexOf(' ' + depthClass + ' ') > -1;
-      if (hasDepthClassAlready)
-      return existingClasses + bg_class;
-      return existingClasses + " " + depthClass + bg_class;
-    });
-  }
+function updateTreeClasses(treePlugin)
+{
+  treePlugin.getPanningContainer().selectAll("g.node")
+  .attr("class", function(data, index, arr) {
+    bg_class = '';
+    var depthClass = "depth-" + data.depth;
+    var existingClasses = this.getAttribute('class');
+    if (data.data.http_status >= 400 || data.data.is_uncommon || data.data.description == 'High') {
+      bg_class = " danger-box "
+    }
+    else if (data.data.http_status > 200 && data.data.http_status < 400 || data.data.description == 'Medium') {
+      bg_class = " warning-box "
+    }
+    else if (data.data.http_status == 200) {
+      bg_class = " success-box "
+    }
+    else if (data.data.description == 'Critical'){
+      bg_class = " critical-box "
+    }
+    else if (data.data.description == 'Low'){
+      bg_class = " low-box "
+    }
+    else if (data.data.description == 'Informational'){
+      bg_class = " info-box "
+    }
+    if (!existingClasses)
+    return depthClass;
+    var hasDepthClassAlready = (' ' + existingClasses + ' ').indexOf(' ' + depthClass + ' ') > -1;
+    if (hasDepthClassAlready)
+    return existingClasses + bg_class;
+    return existingClasses + " " + depthClass + bg_class;
+  });
 }
