4177c4d2e760243ed7bfd2a57a955b780ea7ad3a
Yogesh Ojha <yogesh.ojha11@gmail.com>
2021-07-06 07:20:24 +0530

    Added Organization Model

--- web/targetApp/models.py
+++ web/targetApp/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 
+class Organization(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=300, unique=True)
+    description = models.TextField(blank=True, null=True)
+    targets = models.ManyToManyField('Domain', related_name='targets')
+
+
 class Domain(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=300, unique=True)
