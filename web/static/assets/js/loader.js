7056af837fcb16244e09f03f71dc064752ccb3c3
Yogesh Ojha <yogesh.ojha11@gmail.com>
2021-08-11 13:41:26 +0530

    fixed dark mode

--- web/static/assets/js/loader.js
+++ web/static/assets/js/loader.js
@@ -14,3 +14,19 @@
       });
     });
 })([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+
+
+function changeTheme(){
+   const theme_switch = document.getElementById("theme-switch");
+   if (theme_switch.checked) {
+    DarkReader.setFetchMethod(window.fetch);
+    DarkReader.enable();
+     // Add the ff. line to write to memory.
+     localStorage.setItem("my-theme","dark");
+   }
+   else {
+     DarkReader.disable();
+     // Add the ff. line to write to memory.
+     localStorage.setItem("my-theme",null);
+   }
+ }
