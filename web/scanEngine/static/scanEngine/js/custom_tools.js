ab89a271ad0bd6d5d6f9d6cf134595638235f115
Yogesh Ojha <yogesh.ojha11@gmail.com>
2021-09-01 00:02:11 +0530

    Fixed XSS reported by @phor3nsic on Huntr.dev XSS payloads could be uploaded via Nuclei and GF pattern files

--- web/scanEngine/static/scanEngine/js/custom_tools.js
+++ web/scanEngine/static/scanEngine/js/custom_tools.js
@@ -7,7 +7,7 @@ function load_gf_template(pattern_name){
   $('.modal-text').append(`<div class='outer-div' id="modal-loader"><span class="inner-div spinner-border text-info align-self-center loader-sm"></span></div>`);
   $.getJSON(`/api/getFileContents?gf_pattern&name=${pattern_name}&format=json`, function(data) {
     $('#modal-loader').empty();
-    $('#modal-text-content').append(`<pre>${data['content']}</pre>`);
+    $('#modal-text-content').append(`<pre>${htmlEncode(data['content'])}</pre>`);
   }).fail(function(){
     $('#modal-loader').empty();
     $("#modal-text-content").append(`<p class='text-danger'>Error loading GF Pattern</p>`);
@@ -24,7 +24,7 @@ function load_nuclei_template(pattern_name){
   $('.modal-text').append(`<div class='outer-div' id="modal-loader"><span class="inner-div spinner-border text-info align-self-center loader-sm"></span></div>`);
   $.getJSON(`/api/getFileContents?nuclei_template&name=${pattern_name}&format=json`, function(data) {
     $('#modal-loader').empty();
-    $('#modal-text-content').append(`<pre>${data['content']}</pre>`);
+    $('#modal-text-content').append(`<pre>${htmlEncode(data['content'])}</pre>`);
   }).fail(function(){
     $('#modal-loader').empty();
     $("#modal-text-content").append(`<p class='text-danger'>Error loading Nuclei Template</p>`);
