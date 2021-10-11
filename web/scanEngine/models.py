4b18e8cee6e675c7d4f3ec0b04b3ef1a7a04b14e
Yogesh Ojha <yogesh.ojha11@gmail.com>
2021-10-12 01:29:50 +0530

    Added Secondary Color, modified primary and secondary color

--- web/scanEngine/models.py
+++ web/scanEngine/models.py
@@ -95,7 +95,8 @@ class Hackerone(models.Model):
 
 class VulnerabilityReportSettings(models.Model):
     id = models.AutoField(primary_key=True)
-    primary_color = models.CharField(max_length=10, null=True, blank=True, default='#FF7043')
+    primary_color = models.CharField(max_length=10, null=True, blank=True, default='#FFB74D')
+    secondary_color = models.CharField(max_length=10, null=True, blank=True, default='#212121')
     company_name = models.CharField(max_length=100, null=True, blank=True)
     company_address = models.CharField(max_length=200, null=True, blank=True)
     company_email = models.CharField(max_length=100, null=True, blank=True)
