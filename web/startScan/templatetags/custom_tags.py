9925e6075a2f143cc5d814acd80e2e9cfbaa6006
Yogesh Ojha <yogesh.ojha11@gmail.com>
2021-10-29 00:04:58 +0530

    Added Recent Scan Widget in detail scan

--- web/startScan/templatetags/custom_tags.py
+++ web/startScan/templatetags/custom_tags.py
@@ -26,3 +26,27 @@ def getpath(value):
 @register.filter(name='none_or_never')
 def none_or_never(value):
     return 'Never' if value is None else value
+
+
+# https://stackoverflow.com/a/32801096
+@register.filter
+def next(some_list, current_index):
+    """
+    Returns the next element of the list using the current index if it exists.
+    Otherwise returns an empty string.
+    """
+    try:
+        return some_list[int(current_index) + 1] # access the next element
+    except:
+        return '' # return empty string in case of exception
+
+@register.filter
+def previous(some_list, current_index):
+    """
+    Returns the previous element of the list using the current index if it exists.
+    Otherwise returns an empty string.
+    """
+    try:
+        return some_list[int(current_index) - 1] # access the previous element
+    except:
+        return '' # return empty string in case of exception
