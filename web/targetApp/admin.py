1d7cd0f42f23eff573b23cc0951698724a5ab39a
Yogesh Ojha <yogesh.ojha11@gmail.com>
2021-10-30 16:15:59 +0530

    Added API for Whois Details

--- web/targetApp/admin.py
+++ web/targetApp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Domain, Organization
+from .models import *
 
 admin.site.register(Domain)
 admin.site.register(Organization)
+admin.site.register(RegistrantInfo)
+admin.site.register(WhoisDetail)
+admin.site.register(DomainInfo)
