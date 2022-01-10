a02e2dc595d33658de9ec71bedad4660a53b9752
Yogesh Ojha <yogesh.ojha11@gmail.com>
2022-01-10 22:14:53 +0530

    Successful tasks rightbar implemented

--- web/static/custom/right_sidebar.js
+++ web/static/custom/right_sidebar.js
@@ -32,7 +32,7 @@ function getScanStatusSidebar(reload) {
       for (var scan in scans['scanning']) {
         scan_object = scans['scanning'][scan];
         $('#currently_scanning').append(`
-          <div class="card border-primary border">
+          <div class="card border-primary border mini-card">
           <a href="/scan/detail/${scan_object.id}" class="text-reset item-hovered">
           <div class="card-header bg-soft-primary text-primary mini-card-header">
           ${scan_object.scan_type.engine_name} on ${scan_object.domain.name}
@@ -48,6 +48,7 @@ function getScanStatusSidebar(reload) {
           <span class="">
           Started ${scan_object.elapsed_time} ago.
           </span>
+          </p>
           <div>
           <span class="badge-subdomain-count badge badge-soft-info waves-effect waves-light">&nbsp;&nbsp;${scan_object.subdomain_count}&nbsp;&nbsp;</span>
           <span class="badge-endpoint-count badge badge-soft-primary waves-effect waves-light">&nbsp;&nbsp;${scan_object.endpoint_count}&nbsp;&nbsp;</span>
@@ -56,7 +57,6 @@ function getScanStatusSidebar(reload) {
           <div class="progress mt-2" style="height: 4px;">
           <div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" role="progressbar" aria-valuenow="${scan_object.current_progress}" aria-valuemin="0" aria-valuemax="100" style="width: ${scan_object.current_progress}%"></div>
           </div>
-          </p>
           <a href="#" onclick="stop_scan('${scan_object.celery_id }', true, false)" class="btn btn-xs btn-soft-danger waves-effect waves-light mt-1 float-end"><i class="fe-alert-triangle"></i> Abort</a>
           </div>
           </a>
@@ -88,7 +88,7 @@ function getScanStatusSidebar(reload) {
           }
 
           $('#completed').append(`
-            <div class="card border-${color} border">
+            <div class="card border-${color} border mini-card">
             <a href="/scan/detail/${scan_object.id}" class="text-reset item-hovered float-end">
             <div class="card-header ${bg_color} text-${color} mini-card-header">
             ${scan_object.scan_type.engine_name} on ${scan_object.domain.name}
@@ -123,10 +123,28 @@ function getScanStatusSidebar(reload) {
           bg_color = 'bg-soft-info';
           status_badge = '<span class="float-end badge bg-info">Running</span>';
 
-          $('#currently_running_tasks').append(`<a href="/scan/detail/${task_object.scan_history}" class="mt-2 text-reset item-hovered d-block p-2 ${bg_color}">
-          <p class="text-dark mb-0"><b>${task_name}</b> on ${task_object.subdomain_name}${status_badge}</p>
-          <p class="mb-0"><small>Running Since ${task_object.elapsed_time} ago<small></p>
-          </a>`);
+          $('#currently_running_tasks').append(`
+            <div class="card border-primary border mini-card">
+            <a href="/scan/detail/${task_object.scan_history}" class="text-reset item-hovered">
+            <div class="card-header bg-soft-primary text-primary mini-card-header">
+            ${task_name} on ${task_object.subdomain_name}
+            </div>
+            <div class="card-body mini-card-body">
+            <p class="card-text">
+            <span class="badge badge-soft-primary float-end scan_status">
+            In Progress
+            </span>
+            <span class="">
+            Running Since ${task_object.elapsed_time} ago.
+            </span>
+            </p>
+            <div>
+            </div>
+            <a href="#" onclick="stop_scan('${scan_object.celery_id }', true, false)" class="btn btn-xs btn-soft-danger waves-effect waves-light mt-1 float-end"><i class="fe-alert-triangle"></i> Abort</a>
+            </div>
+            </a>
+            </div>
+          `);
         }
       }
       else{
@@ -138,23 +156,39 @@ function getScanStatusSidebar(reload) {
           task_object = tasks['completed'][task];
           task_name = get_task_name(task_object);
           if (task_object.status == 0 ) {
+            color = 'danger';
             bg_color = 'bg-soft-danger';
             status_badge = '<span class="float-end badge bg-danger">Failed</span>';
           }
           else if (task_object.status == 3) {
+            color = 'danger';
             bg_color = 'bg-soft-danger';
             status_badge = '<span class="float-end badge bg-danger">Aborted</span>';
           }
           else if (task_object.status == 2){
+            color = 'success';
             bg_color = 'bg-soft-success';
             status_badge = '<span class="float-end badge bg-success">Task Completed</span>';
           }
 
-          $('#completed_tasks').append(`<a href="/scan/detail/${task_object.scan_history}" class="mt-2 text-reset item-hovered d-block p-2 ${bg_color}">
-          <p class="text-dark mb-0"><b>${task_name}</b> on ${task_object.subdomain_name}${status_badge}</p>
-          <p class="mb-0"><small>Task Completed ${task_object.completed_ago} ago<small></p>
-          <p class="mb-0"><small>Took ${task_object.time_taken}<small></p>
-          </a>`);
+          $('#completed_tasks').append(`
+            <div class="card border-${color} border mini-card">
+            <a href="/scan/detail/${task_object.scan_history}" class="text-reset item-hovered">
+            <div class="card-header ${bg_color} text-${color} mini-card-header">
+            ${task_name} on <b>${task_object.subdomain_name}</b>
+            </div>
+            <div class="card-body mini-card-body">
+            <p class="card-text">
+            ${status_badge}
+            <span class="">
+            Task Completed ${task_object.completed_ago} ago
+            </span>
+            Took ${task_object.time_taken}
+            </p>
+            </div>
+            </a>
+            </div>
+          `);
         }
       }
       else{
@@ -206,23 +240,23 @@ function getScanStatusSidebar(reload) {
   }
 
 
-  function get_task_name(data){
-    if (data['dir_file_fuzz']) {
-      return 'Directory Fuzzing';
-    }
-    else if (data['port_scan']) {
-      return 'Port Scan';
-    }
-    else if (data['fetch_url']) {
-      return 'Endpoint Gathering';
-    }
-    else if (data['vulnerability_scan']) {
-      return 'Vulnerability Scan';
-    }
-    else if (data['osint']) {
-      return 'OSINT';
-    }
-    else{
-      return 'Unknown';
-    }
+function get_task_name(data){
+  if (data['dir_file_fuzz']) {
+    return 'Directory Fuzzing';
   }
+  else if (data['port_scan']) {
+    return 'Port Scan';
+  }
+  else if (data['fetch_url']) {
+    return 'Endpoint Gathering';
+  }
+  else if (data['vulnerability_scan']) {
+    return 'Vulnerability Scan';
+  }
+  else if (data['osint']) {
+    return 'OSINT';
+  }
+  else{
+    return 'Unknown';
+  }
+}
