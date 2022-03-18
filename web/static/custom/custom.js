aefa8bdf7af6de63bfc4674ed362e231c3d9ddbf
Yogesh Ojha <yogesh.ojha11@gmail.com>
2022-03-19 01:53:02 +0530

    Added Directory Rendering

--- web/static/custom/custom.js
+++ web/static/custom/custom.js
@@ -970,56 +970,7 @@ function show_subscan_results(subscan_id) {
 						</div>
 					`);
 				} else {
-					$('#xl-modal-content').append(`<h5> ${response['result'][0]['directory_files'].length} Directories Discovered on subdomain ${response['subscan']['subdomain_name']}</h5>`);
-					$('#xl-modal-content').append(`
-						<div class="">
-						<table id="directory-modal-datatable" class="table dt-responsive nowrap w-100">
-						<thead>
-						<tr>
-						<th>Directory</th>
-						<th class="text-center">HTTP Status</th>
-						<th>Content Length</th>
-						<th>Lines</th>
-						<th>Words</th>
-						</tr>
-						</thead>
-						<tbody id="directory_tbody">
-						</tbody>
-						</table>
-						</div>
-					`);
-					$('#directory_tbody').empty();
-					for(var dir_obj in response['result'][0]['directory_files']) {
-						var dir = response['result'][0]['directory_files'][dir_obj];
-						$('#directory_tbody').append(`
-							<tr>
-							<td><a href="${dir.url}" target="_blank">${dir.name}</a></td>
-							<td class="text-center">${get_http_status_badge(dir.http_status)}</td>
-							<td>${dir.length}</td>
-							<td>${dir.lines}</td>
-							<td>${dir.words}</td>
-							</tr>
-						`);
-					}
-					$("#directory-modal-datatable").DataTable({
-						"oLanguage": {
-							"oPaginate": {
-								"sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>',
-								"sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>'
-							},
-							"sInfo": "Showing page _PAGE_ of _PAGES_",
-							"sSearch": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
-							"sSearchPlaceholder": "Search...",
-							"sLengthMenu": "Results :  _MENU_",
-						},
-						"dom": "<'dt--top-section'<'row'<'col-12 col-sm-6 d-flex justify-content-sm-start justify-content-center'f><'col-12 col-sm-6 d-flex justify-content-sm-end justify-content-center'l>>>" + "<'table-responsive'tr>" + "<'dt--bottom-section d-sm-flex justify-content-sm-between text-center'<'dt--pages-count  mb-sm-0 mb-3'i><'dt--pagination'p>>",
-						"order": [
-							[2, "desc"]
-						],
-						drawCallback: function() {
-							$(".dataTables_paginate > .pagination").addClass("pagination-rounded")
-						}
-					});
+					render_directories_in_xl_modal(response['result'][0]['directory_files'].length, response['subscan']['subdomain_name'], response['result'][0]['directory_files']);
 				}
 			}
 		} else {
@@ -1118,7 +1069,6 @@ function render_endpoint_in_xlmodal(endpoint_count, subdomain_name, result) {
 		}
 	});
 }
-
 
 function render_vulnerability_in_xl_modal(vuln_count, subdomain_name, result){
 	// This function will render the vulnerability datatable in xl modal
@@ -1272,6 +1222,59 @@ function render_vulnerability_in_xl_modal(vuln_count, subdomain_name, result){
 		"dom": "<'dt--top-section'<'row'<'col-12 col-sm-6 d-flex justify-content-sm-start justify-content-center'f><'col-12 col-sm-6 d-flex justify-content-sm-end justify-content-center'l>>>" + "<'table-responsive'tr>" + "<'dt--bottom-section d-sm-flex justify-content-sm-between text-center'<'dt--pages-count  mb-sm-0 mb-3'i><'dt--pagination'p>>",
 		"order": [
 			[5, "desc"]
+		],
+		drawCallback: function() {
+			$(".dataTables_paginate > .pagination").addClass("pagination-rounded")
+		}
+	});
+}
+
+function render_directories_in_xl_modal(directory_count, subdomain_name, result){
+	$('#xl-modal-content').append(`<h5> ${directory_count} Directories Discovered on subdomain ${subdomain_name}</h5>`);
+	$('#xl-modal-content').append(`
+		<div class="">
+		<table id="directory-modal-datatable" class="table dt-responsive nowrap w-100">
+		<thead>
+		<tr>
+		<th>Directory</th>
+		<th class="text-center">HTTP Status</th>
+		<th>Content Length</th>
+		<th>Lines</th>
+		<th>Words</th>
+		</tr>
+		</thead>
+		<tbody id="directory_tbody">
+		</tbody>
+		</table>
+		</div>
+	`);
+	$('#directory_tbody').empty();
+	for(var dir_obj in result) {
+		var dir = result[dir_obj];
+		$('#directory_tbody').append(`
+			<tr>
+			<td><a href="${dir.url}" target="_blank">${dir.name}</a></td>
+			<td class="text-center">${get_http_status_badge(dir.http_status)}</td>
+			<td>${dir.length}</td>
+			<td>${dir.lines}</td>
+			<td>${dir.words}</td>
+			</tr>
+		`);
+	}
+	$("#directory-modal-datatable").DataTable({
+		"oLanguage": {
+			"oPaginate": {
+				"sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>',
+				"sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>'
+			},
+			"sInfo": "Showing page _PAGE_ of _PAGES_",
+			"sSearch": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
+			"sSearchPlaceholder": "Search...",
+			"sLengthMenu": "Results :  _MENU_",
+		},
+		"dom": "<'dt--top-section'<'row'<'col-12 col-sm-6 d-flex justify-content-sm-start justify-content-center'f><'col-12 col-sm-6 d-flex justify-content-sm-end justify-content-center'l>>>" + "<'table-responsive'tr>" + "<'dt--bottom-section d-sm-flex justify-content-sm-between text-center'<'dt--pages-count  mb-sm-0 mb-3'i><'dt--pagination'p>>",
+		"order": [
+			[2, "desc"]
 		],
 		drawCallback: function() {
 			$(".dataTables_paginate > .pagination").addClass("pagination-rounded")
