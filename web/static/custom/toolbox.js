aad0c265640dbdc179525827fb015d3fa6e65997
Yogesh Ojha <yogesh.ojha11@gmail.com>
2022-04-16 00:40:16 +0530

    Added whois lookup modal in toolbox

--- web/static/custom/toolbox.js
+++ web/static/custom/toolbox.js
@@ -7,7 +7,7 @@ function show_whois_lookup_modal(){
 			<input class="form-control" type="text" id="whois_domain_name" required="" placeholder="yourdomain.com">
 		</div>
 		<div class="mb-3 text-center">
-			<button class="btn btn-primary float-end" type="submit" id="search_whois_toolbox_btn" onclick="toolbox_lookup_whois()">Search</button>
+			<button class="btn btn-primary float-end" type="submit" id="search_whois_toolbox_btn" onclick="toolbox_lookup_whois()">Search Whois</button>
 		</div>
 	`);
 	$('#modal_dialog').modal('show');
@@ -16,7 +16,7 @@ function show_whois_lookup_modal(){
 function toolbox_lookup_whois(){
 	var domain = document.getElementById("whois_domain_name").value;
 	if (domain) {
-		get_domain_whois(domain);
+		get_domain_whois(domain, show_add_target_btn=true);
 	}
 	else{
 		swal.fire("Error!", 'Please enter the domain/IP Address!', "warning", {
