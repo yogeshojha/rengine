dc4ed9a3fdeb8481a57e010c641ae4196ca157d3
Yogesh Ojha <yogesh.ojha11@gmail.com>
2022-04-23 10:52:16 +0530

    changed eyewitness to screenshot in yaml

--- web/reNgine/definitions.py
+++ web/reNgine/definitions.py
@@ -19,7 +19,7 @@ ALL = 'all'
 
 SUBDOMAIN_DISCOVERY = 'subdomain_discovery'
 PORT_SCAN = 'port_scan'
-EYEWITNESS = 'eyewitness'
+SCREENSHOT = 'screenshot'
 DIR_FILE_FUZZ = 'dir_file_fuzz'
 FETCH_URL = 'fetch_url'
 INTENSITY = 'intensity'
