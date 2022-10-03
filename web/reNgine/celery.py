be6d63b00c448cd9cf1c1109bca36d16c65cd739
ocervell <olivier.cervello@gmail.com>
2022-10-03 13:05:30 +0200

    Update logging settings to be nicer

--- web/reNgine/celery.py
+++ web/reNgine/celery.py
@@ -2,11 +2,7 @@ import os
 
 import django
 from celery import Celery
-from celery.app import trace
-from celery.app.log import TaskFormatter
-from celery.signals import after_setup_task_logger
-
-trace.LOG_SUCCESS = 'Task %(name)s[%(id)s] succeeded in %(runtime)ss'
+from celery.signals import setup_logging
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reNgine.settings')
 django.setup()
@@ -16,7 +12,8 @@ app = Celery('reNgine')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
-@after_setup_task_logger.connect
-def setup_task_logger(logger, *args, **kwargs):
-	for handler in logger.handlers:
-		handler.setFormatter(TaskFormatter('%(task_name)s | %(message)s'))
+
+@setup_logging.connect()
+def config_loggers(*args, **kwargs):
+    from logging.config import dictConfig
+    dictConfig(app.conf['LOGGING'])
