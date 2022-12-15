8228234e1766c0724f8c64739b5fb984acfc65cc
Olivier Cervello <olivier.cervello@gmail.com>
2022-12-15 16:01:58 +0100

    clean Dockerfile and entrypoint for faster set up

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z ${POSTGRES_HOST} ${POSTGRES_PORT}; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
 python3 manage.py migrate
-
 python3 manage.py runserver 0.0.0.0:8000
 
 exec "$@"
