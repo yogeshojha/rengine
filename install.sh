65b7dcd0e383b2364e32cb09ee4b573b5d064062
AnonymousWP <50231698+AnonymousWP@users.noreply.github.com>
2023-04-18 12:20:46 +0200

    fix(migrations): revert sudo and use `echo`
    Using `make migrate` right away did not work, tried multiple things.

--- install.sh
+++ install.sh
@@ -123,9 +123,9 @@ if [ "${failed}" -eq 0 ]; then
   echo "Creating an account"
   echo "#########################################################################"
   make username
-  make migrate
 
   tput setaf 2 && printf "\n%s\n" "Thank you for installing reNgine, happy recon!!"
+  echo "In case you have unapplied migrations (see above in red), run 'make migrate'"
 else
   tput setaf 1 && printf "\n%s\n" "reNgine installation failed!!"
 fi
