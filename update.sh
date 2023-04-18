8500c5b40f99af2a5554b88fe4513dbd679ad72f
AnonymousWP <50231698+AnonymousWP@users.noreply.github.com>
2023-04-18 09:37:52 +0200

    ops(`update.sh`): add choices

--- update.sh
+++ update.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-make down && git pull && make build && make up
+echo "Do you want to apply your local changes after updating? (y/n)"
+read answer
+
+if [[ $answer == "y" ]]; then
+  make down && git stash save && git pull && git stash apply && make build && make up
+elif [[ $answer == "n" ]]; then
+  make down && git stash && git stash drop && git pull && make build && make up
+else
+  echo "Invalid input. Please enter 'y' or 'n'."
+fi
