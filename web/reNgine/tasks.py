737ea09e6efdac849b82e08becf9e3606ec90afc
Yogesh Ojha <yogesh.ojha11@gmail.com>
2023-04-26 22:47:01 +0530

    fix nmap script

--- web/reNgine/tasks.py
+++ web/reNgine/tasks.py
@@ -1232,7 +1232,7 @@ def port_scan(self, hosts=[], ctx={}, description=None):
 	# nmap args
 	nmap_enabled = config.get(ENABLE_NMAP, False)
 	nmap_cmd = config.get(NMAP_COMMAND, '')
-	nmap_script = config.get(NMAP_SCRIPT)
+	nmap_script = config.get(NMAP_SCRIPT, '')
 	nmap_script = ','.join(nmap_script)
 	nmap_script_args = config.get(NMAP_SCRIPT_ARGS)
 
