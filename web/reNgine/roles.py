3f94fd4eff5e1b92ba9918c14e45fca79b89f55e
Yogesh Ojha <yogesh.ojha11@gmail.com>
2023-06-18 18:25:25 +0530

    Added Permissions to ui frontend

--- web/reNgine/roles.py
+++ web/reNgine/roles.py
@@ -5,9 +5,6 @@ class Admin(AbstractUserRole):
     available_permissions = {
         PERM_ADD_MODIFY_SYSTEM_SETTINGS: True,
         PERM_ADD_MODIFY_SCAN_SETTINGS: True,
-        PERM_ADD_MODIFY_SCAN_ENGINES: True,
-        PERM_ADD_MODIFY_EXTERNAL_TOOLS: True,
-        PERM_ADD_MODIFY_TARGET: True,
         PERM_VIEW_DOWNLOAD_REPORT: True,
         PERM_INITATE_SCANS: True,
     }
@@ -17,9 +14,6 @@ class PenetrationTester(AbstractUserRole):
     available_permissions = {
         PERM_ADD_MODIFY_SYSTEM_SETTINGS: False,
         PERM_ADD_MODIFY_SCAN_SETTINGS: True,
-        PERM_ADD_MODIFY_SCAN_ENGINES: True,
-        PERM_ADD_MODIFY_EXTERNAL_TOOLS: True,
-        PERM_ADD_MODIFY_TARGET: True,
         PERM_VIEW_DOWNLOAD_REPORT: True,
         PERM_INITATE_SCANS: True,
     }
@@ -29,9 +23,6 @@ class Auditor(AbstractUserRole):
     available_permissions = {
         PERM_ADD_MODIFY_SYSTEM_SETTINGS: False,
         PERM_ADD_MODIFY_SCAN_SETTINGS: False,
-        PERM_ADD_MODIFY_SCAN_ENGINES: False,
-        PERM_ADD_MODIFY_EXTERNAL_TOOLS: False,
-        PERM_ADD_MODIFY_TARGET: False,
         PERM_VIEW_DOWNLOAD_REPORT: True,
         PERM_INITATE_SCANS: False,
     }
