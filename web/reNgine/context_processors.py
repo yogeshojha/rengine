b7cc80cc86d94f23e0be52fd657213c39c6907ed
Yogesh Ojha <yogesh.ojha11@gmail.com>
2023-06-30 00:53:04 +0530

    Added current project in context pre processor

--- web/reNgine/context_processors.py
+++ web/reNgine/context_processors.py
@@ -2,4 +2,12 @@ from dashboard.models import *
 
 def projects(request):
     projects = Project.objects.all()
-    return {'projects': projects}
+    try:
+        slug = request.resolver_match.kwargs.get('slug')
+        project = Project.objects.get(slug=slug)
+    except Exception as e:
+        project = None
+    return {
+        'projects': projects,
+        'current_project': project
+    }
