7106022137fa596ea5bd7af2084d651f86fd9ed8
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2023-07-21 00:25:16 +0530

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,35 +1,87 @@
 @echo off
 
-:: Credits: https://github.com/ninjhacks
-
-set COMPOSE_ALL_FILES  = -f docker-compose.yml
-set SERVICES           = db web proxy redis celery celery-beat
+set "COMPOSE_ALL_FILES=-f docker-compose.yml"
+set "SERVICES=db web proxy redis celery celery-beat"
 
 :: Generate certificates.
-if "%1" == "certs" docker-compose -f docker-compose.setup.yml run --rm certs
-:: Generate certificates.
-if "%1" == "setup" docker-compose -f docker-compose.setup.yml run --rm certs
+if "%1" == "certs" (
+    docker-compose -f docker-compose.setup.yml run --rm certs
+    goto :EOF
+)
+
 :: Build and start all services.
-if "%1" == "up" docker-compose %COMPOSE_ALL_FILES% up -d --build %SERVICES%
+if "%1" == "up" (
+    docker-compose %COMPOSE_ALL_FILES% up -d --build %SERVICES%
+    goto :EOF
+)
+
 :: Build all services.
-if "%1" == "build" docker-compose %COMPOSE_ALL_FILES% build %SERVICES%
+if "%1" == "build" (
+    docker-compose %COMPOSE_ALL_FILES% build %SERVICES%
+    goto :EOF
+)
+
 :: Generate Username (Use only after make up).
-if "%1" == "username" docker-compose %COMPOSE_ALL_FILES% exec web python3 manage.py createsuperuser
+if "%1" == "username" (
+    docker-compose %COMPOSE_ALL_FILES% exec web python3 manage.py createsuperuser
+    goto :EOF
+)
+
 :: Pull Docker images.
-if "%1" == "pull" docker login docker.pkg.github.com & docker-compose %COMPOSE_ALL_FILES% pull
+if "%1" == "pull" (
+    docker login docker.pkg.github.com
+    docker-compose %COMPOSE_ALL_FILES% pull %SERVICES%
+    goto :EOF
+)
+
 :: Down all services.
-if "%1" == "down" docker-compose %COMPOSE_ALL_FILES% down
+if "%1" == "down" (
+    docker-compose %COMPOSE_ALL_FILES% down
+    goto :EOF
+)
+
 :: Stop all services.
-if "%1" == "stop" docker-compose %COMPOSE_ALL_FILES% stop %SERVICES%
+if "%1" == "stop" (
+    docker-compose %COMPOSE_ALL_FILES% stop %SERVICES%
+    goto :EOF
+)
+
 :: Restart all services.
-if "%1" == "restart" docker-compose %COMPOSE_ALL_FILES% restart %SERVICES%
+if "%1" == "restart" (
+    docker-compose %COMPOSE_ALL_FILES% restart %SERVICES%
+    goto :EOF
+)
+
 :: Remove all services containers.
-if "%1" == "rm" docker-compose %COMPOSE_ALL_FILES% rm -f %SERVICES%
+if "%1" == "rm" (
+    docker-compose %COMPOSE_ALL_FILES% rm -f %SERVICES%
+    goto :EOF
+)
+
 :: Tail all logs with -n 1000.
-if "%1" == "logs" docker-compose %COMPOSE_ALL_FILES% logs --follow --tail=1000 %SERVICES%
+if "%1" == "logs" (
+    docker-compose %COMPOSE_ALL_FILES% logs --follow --tail=1000 %SERVICES%
+    goto :EOF
+)
+
 :: Show all Docker images.
-if "%1" == "images" docker-compose %COMPOSE_ALL_FILES% images %SERVICES%
+if "%1" == "images" (
+    docker-compose %COMPOSE_ALL_FILES% images %SERVICES%
+    goto :EOF
+)
+
 :: Remove containers and delete volume data.
-if "%1" == "prune" docker-compose %COMPOSE_ALL_FILES% stop %SERVICES% & docker-compose %COMPOSE_ALL_FILES% rm -f %SERVICES% & docker volume prune -f
+if "%1" == "prune" (
+    docker-compose %COMPOSE_ALL_FILES% stop %SERVICES%
+    docker-compose %COMPOSE_ALL_FILES% rm -f %SERVICES%
+    docker volume prune -f
+    goto :EOF
+)
+
 :: Show this help.
-if "%1" == "help" @echo Make application docker images and manage containers using docker-compose files only for windows.
+if "%1" == "help" (
+    echo Make application docker images and manage containers using docker-compose files only for windows.
+    goto :EOF
+)
+
+goto :EOF
