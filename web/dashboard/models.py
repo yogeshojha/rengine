417a9254ea44cf24a5c03584c341b588eeb754df
Yogesh Ojha <yogesh.ojha11@gmail.com>
2023-07-25 11:27:14 +0530

    Added API keys model

--- web/dashboard/models.py
+++ web/dashboard/models.py
@@ -16,3 +16,19 @@ class Project(models.Model):
 
 	def __str__(self):
 		return self.slug
+
+
+class OpenAiAPIKey(models.Model):
+	id = models.AutoField(primary_key=True)
+	key = models.CharField(max_length=500)
+
+	def __str__(self):
+		return self.key
+
+
+class NetlasAPIKey(models.Model):
+	id = models.AutoField(primary_key=True)
+	key = models.CharField(max_length=500)
+
+	def __str__(self):
+		return self.key
