7f884874300c108912b2ca99122677b048a51f49
Yogesh Ojha <yogesh.ojha11@gmail.com>
2023-08-15 11:44:44 +0530

    Added Subdomain Grouping

--- web/static/custom/vuln_datatables.js
+++ web/static/custom/vuln_datatables.js
@@ -42,15 +42,15 @@ const vuln_datatable_length_menu = [[50, 100, 500, 1000, -1], [50, 100, 500, 100
 
 function vulnerability_datatable_col_visibility(table){
 	if(!$('#vuln_source_checkbox').is(":checked")){
-		table.column(get_datatable_col_index('vuln_source_checkbox')).visible(false);
+		table.column(get_datatable_col_index('vuln_source_checkbox', vuln_datatable_columns)).visible(false);
 	}
 	if(!$('#vuln_severity_checkbox').is(":checked")){
-		table.column(get_datatable_col_index('severity')).visible(false);
+		table.column(get_datatable_col_index('severity', vuln_datatable_columns)).visible(false);
 	}
 	if(!$('#vuln_vulnerable_url_checkbox').is(":checked")){
-		table.column(get_datatable_col_index('http_url')).visible(false);
+		table.column(get_datatable_col_index('http_url', vuln_datatable_columns)).visible(false);
 	}
 	if(!$('#vuln_status_checkbox').is(":checked")){
-		table.column(get_datatable_col_index('status')).visible(false);
+		table.column(get_datatable_col_index('status', vuln_datatable_columns)).visible(false);
 	}
 }
