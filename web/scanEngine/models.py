215543f622139b8ba6b69c8209b64e28a9a4f8bd
Raynald <raynald@easi-services.fr>
2023-10-18 16:31:07 +0200

    Permit larger command in db

--- web/scanEngine/models.py
+++ web/scanEngine/models.py
@@ -123,7 +123,7 @@ class InstalledExternalTool(models.Model):
     description = models.CharField(max_length=2000)
     github_url = models.CharField(max_length=500)
     license_url = models.CharField(max_length=500, null=True, blank=True)
-    version_lookup_command = models.CharField(max_length=100, null=True, blank=True)
+    version_lookup_command = models.CharField(max_length=200, null=True, blank=True)
     update_command = models.CharField(max_length=200, null=True, blank=True)
     install_command = models.CharField(max_length=200)
     version_match_regex = models.CharField(max_length=100, default='[vV]*(\d+\.)?(\d+\.)?(\*|\d+)', null=True, blank=True)
