2af3b339c52247996e0bd8ec4e7e91d641144380
Raynald <raynald@easi-services.fr>
2024-02-14 19:36:20 +0100

    Add remote debug configuration in dev env

--- web/celery-entrypoint-dev.sh
+++ web/celery-entrypoint-dev.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-# Django debug toolbar
-pip install django-debug-toolbar
+if [ "$DEBUG" == "1" ]; then
+    # Django debug toolbar
+    pip install django-debug-toolbar
+fi
+
+# Check if remote debugging is enabled and set concurrency to 1 for easier debug
+if [ "$REMOTE_DEBUG" == "1" ]; then
+    # Live debug
+    pip install debugpy
+
+    # To debug opened port with netstat
+    apt install net-tools -y
+
+    # Set celery concurrency to 1 because thread processes is hard to debug
+    export MIN_CONCURRENCY=1
+    export MAX_CONCURRENCY=1
+fi
 
 ./celery-entrypoint.sh
--- web/reNgine/tasks.py
+++ web/reNgine/tasks.py
@@ -35,6 +35,8 @@ from scanEngine.models import (EngineType, InstalledExternalTool, Notification, 
 from startScan.models import *
 from startScan.models import EndPoint, Subdomain, Vulnerability
 from targetApp.models import Domain
+if REMOTE_DEBUG:
+	import debugpy
 
 """
 Celery tasks.
@@ -70,6 +72,9 @@ def initiate_scan(
 		out_of_scope_subdomains (list): Out-of-scope subdomains.
 		url_filter (str): URL path. Default: ''
 	"""
+
+	if REMOTE_DEBUG:
+		debug()
 
 	# Get scan history
 	scan = ScanHistory.objects.get(pk=scan_history_id)
@@ -219,6 +224,9 @@ def initiate_subscan(
 		results_dir (str): Results directory.
 		url_filter (str): URL path. Default: ''
 	"""
+
+	if REMOTE_DEBUG:
+		debug()
 
 	# Get Subdomain, Domain and ScanHistory
 	subdomain = Subdomain.objects.get(pk=subdomain_id)
@@ -4770,3 +4778,18 @@ def gpt_vulnerability_description(vulnerability_id):
 			vuln.save()
 
 	return response
+
+#----------------------#
+#     Remote debug     #
+#----------------------#
+
+def debug():
+	try:
+		# Activate remote debug for scan worker
+		if REMOTE_DEBUG:
+				logger.error('================= Debug activated, wainting attach =============')
+				os.environ['GEVENT_SUPPORT'] = 'True'
+				debugpy.listen(('0.0.0.0',5679))
+				debugpy.wait_for_client()
+	except  Exception as e:
+		logger.error(e)
