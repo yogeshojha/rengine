914792872e973c04ac894ac7e8cb0691ca502677
Quentin POIRIER <q.poirier@astar.services>
2024-05-10 15:14:27 +0200

    fixing Makefiles & beat entrypoint to allow for proper initialization on first startup

--- web/beat-entrypoint.sh
+++ web/beat-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-poetry run -C $HOME/ python3 manage.py collectstatic
+poetry run -C $HOME/ python3 manage.py collectstatic --no-input
 poetry run -C $HOME/ python3 manage.py migrate
 
-celery -A reNgine beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
+poetry run -C $HOME/ python3 manage.py loaddata fixtures/default_scan_engines.yaml --app scanEngine.EngineType
+poetry run -C $HOME/ python3 manage.py loaddata fixtures/default_keywords.yaml --app scanEngine.InterestingLookupModel
+poetry run -C $HOME/ python3 manage.py loaddata fixtures/external_tools.yaml --app scanEngine.InstalledExternalTool
+
+poetry run -C $HOME/ celery -A reNgine beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
 
 exec "$@"
