e70749310a260e216e6978c5ff7316bf397043cc
psyray <psyray+github@free.fr>
2024-05-21 22:22:04 +0200

    Fix techs endpoint error

--- web/startScan/models.py
+++ web/startScan/models.py
@@ -363,7 +363,7 @@ class EndPoint(models.Model):
 	webserver = models.CharField(max_length=1000, blank=True, null=True)
 	is_default = models.BooleanField(null=True, blank=True, default=False)
 	matched_gf_patterns = models.CharField(max_length=10000, null=True, blank=True)
-	techs = models.ManyToManyField('Technology', related_name='techs', null=True, blank=True)
+	techs = models.ManyToManyField('Technology', related_name='techs', blank=True)
 	# used for subscans
 	endpoint_subscan_ids = models.ManyToManyField('SubScan', related_name='endpoint_subscan_ids', blank=True)
 
