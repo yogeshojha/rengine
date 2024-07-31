6733b62cf97dd524a4d92cfb65be0727335ae38c
Yogesh Ojha <yogesh.ojha11@gmail.com>
2024-07-31 19:57:47 +0530

    remove external ip from dashboard

--- web/reNgine/context_processors.py
+++ web/reNgine/context_processors.py
@@ -1,5 +1,5 @@
 from dashboard.models import *
-import requests
+
 
 def projects(request):
     projects = Project.objects.all()
@@ -11,10 +11,4 @@ def projects(request):
     return {
         'projects': projects,
         'current_project': project
-    }
-
-def misc(request):
-    externalIp = requests.get('https://checkip.amazonaws.com').text.strip()
-    return {
-        'external_ip': externalIp
     }
