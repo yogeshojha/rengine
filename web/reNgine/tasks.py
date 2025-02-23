2ae822b5c4729e1da86ab900220968425d725971
David McHale <dmchale.dev@gmail.com>
2025-02-23 04:56:37 +0000

    #1476-#1480-#1487 fixes, adding fallback for template-url variable, fixing missing comma, fixing encodedURLData typo

--- web/reNgine/tasks.py
+++ web/reNgine/tasks.py
@@ -3671,7 +3671,7 @@ def parse_nuclei_result(line):
 		'type': line['type'],
 		'severity': NUCLEI_SEVERITY_MAP[line['info'].get('severity', 'unknown')],
 		'template': line['template'],
-		'template_url': line['template-url'],
+		'template_url': line.get('template-url', []),
 		'template_id': line['template-id'],
 		'description': line['info'].get('description', ''),
 		'matcher_name': line.get('matcher-name', ''),
