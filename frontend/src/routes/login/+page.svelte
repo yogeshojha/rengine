2b69dec70ee6403e24150ee9c7943b0b8a8260f4
Yogesh Ojha <yogesh.ojha11@gmail.com>
2026-01-27 19:08:16 +0530

    dark login always

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -3,16 +3,18 @@
 	import { Boxes } from 'lucide-svelte';
 </script>
 
-<div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-	<div class="flex w-full max-w-sm flex-col gap-6">
-		<a href="##" class="flex items-center gap-2 self-center font-medium">
-			<div
-				class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
-			>
-				<Boxes class="size-4" />
-			</div>
-			reNgine
-		</a>
-		<LoginForm />
+<div class="dark">
+	<div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+		<div class="flex w-full max-w-sm flex-col gap-6">
+			<a href="##" class="flex items-center gap-2 self-center font-medium text-white">
+				<div
+					class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
+				>
+					<Boxes class="size-4" />
+				</div>
+				reNgine
+			</a>
+			<LoginForm />
+		</div>
 	</div>
 </div>
