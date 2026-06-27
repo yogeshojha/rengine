83dde7842adfaa580cd11655d6cdd7c17f022650
Vi Manh Tuong <contact@manhtuong.net>
2026-06-28 02:15:51 +0700

    Update install.sh to dynamically fetch the latest Docker Compose version for installation

--- install.sh
+++ install.sh
@@ -93,9 +93,10 @@ echo "#########################################################################"
 if [ -x "$(command -v docker compose)" ]; then
   tput setaf 2; echo "Docker Compose already installed, skipping."
 else
-  curl -L "https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
-  ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+  COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+  sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
   tput setaf 2; echo "Docker Compose installed!!!"
 fi
 
